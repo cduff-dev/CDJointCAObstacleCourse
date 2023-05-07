8dad78e863ba2db4c4b60668ba85ea714fd76c34
cduff-dev <57147137+cduff-dev@users.noreply.github.com>
2023-05-07 21:35:39 +0100

    Added Minimap, Added Main menu and main menu functionality, Added Options Menu, Added Win screen and functionality, Added Loss Screen and Functionality, Added win, loss, and button sound effects, Fixed light bug with pickups.
    Added Minimap, Added Main menu and main menu functionality, Added Options Menu, Added Win screen and functionality, Added Loss Screen and Functionality, Added win, loss, and button sound effects, Fixed light bug with pickups.

---
+++ CDObstacleCourse/Intermediate/Build/Win64/CDObstacleCourse/Development/Core/SharedPCH.Core.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.35.32217
+#include "C:/Users/charl/Documents/CDJointCAObstacleCourse/CDObstacleCourse/Intermediate/Build/Win64/CDObstacleCourse/Development/Core/SharedPCH.Core.ShadowErrors.h"
